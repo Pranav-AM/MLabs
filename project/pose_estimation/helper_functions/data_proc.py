86b85548f9b0e773cf86ce58cc36fe0266c36522
Bharat Reddy <bharat8apr@gmail.com>
2019-04-20 11:26:12 +0530

    Pose Estimation directory modified

--- project/pose_estimation/helper_functions/data_proc.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import os
-data=[]
-for file in sorted(os.listdir('./rawdata')):
-    d1=pickle.load(open( './rawdata/'+file, "rb" ))
-    print(len(d1))
-    data.append(d1)
-print(len(data))
